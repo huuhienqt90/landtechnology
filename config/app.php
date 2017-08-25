70759ce55e1797ec75786f9d40fdfb75348b9452
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2017-08-25 21:19:52 +0700

    Install some package

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -224,6 +226,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Module' => Nwidart\Modules\Facades\Module::class,
 
     ],
 
