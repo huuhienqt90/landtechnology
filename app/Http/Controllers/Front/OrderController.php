fe815606800321ee061ba716d0f1eb29572f150b
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2017-09-11 20:59:17 +0700

    Update

---
+++ app/Http/Controllers/Front/OrderController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    /**
+     * Show checkout thank you page
+     */
+    public function showCheckoutThankYou(Request $request){
+        $GECDResult = $this->PayPal->GetExpressCheckoutDetails($request->token);
+
+        $DECPFields = array(
+            'token' => $request->token,
+            'payerid' => $GECDResult['PAYERID']
+        );
+
+        $Payments = array();
+        $Payment = array(
+            'amt' => Cart::total(),                          // Required.  The total cost of the transaction to the customer.  If shipping cost and tax charges are known, include them in this value.  If not, this value should be the current sub-total of the order.
+            'currencycode' => 'USD',                    // A three-character currency code.  Default is USD.
+            'itemamt' => Cart::total(),                       // Required if you specify itemized L_AMT fields. Sum of cost of all items in this order.
+            'shippingamt' => 0,                   // Total shipping costs for this order.  If you specify SHIPPINGAMT you mut also specify a value for ITEMAMT.
+            'insuranceoptionoffered' => '',         // If true, the insurance drop-down on the PayPal review page displays the string 'Yes' and the insurance amount.  If true, the total shipping insurance for this order must be a positive number.
+            'handlingamt' => '',                    // Total handling costs for this order.  If you specify HANDLINGAMT you mut also specify a value for ITEMAMT.
+            'taxamt' => Cart::tax(),                         // Required if you specify itemized L_TAXAMT fields.  Sum of all tax items in this order.
+            'desc' => 'This is a test order.',                          // Description of items on the order.  127 char max.
+            'custom' => '',                         // Free-form field for your own use.  256 char max.
+            'invnum' => '',                         // Your own invoice or tracking number.  127 char max.
+            'notifyurl' => '',                          // URL for receiving Instant Payment Notifications
+            'shiptoname' => '',                     // Required if shipping is included.  Person's name associated with this address.  32 char max.
+            'shiptostreet' => '',                   // Required if shipping is included.  First street address.  100 char max.
+            'shiptostreet2' => '',                  // Second street address.  100 char max.
+            'shiptocity' => '',                     // Required if shipping is included.  Name of city.  40 char max.
+            'shiptostate' => '',                    // Required if shipping is included.  Name of state or province.  40 char max.
+            'shiptozip' => '',                      // Required if shipping is included.  Postal code of shipping address.  20 char max.
+            'shiptocountry' => '',                  // Required if shipping is included.  Country code of shipping address.  2 char max.
+            'shiptophonenum' => '',                 // Phone number for shipping address.  20 char max.
+            'notetext' => 'This is a test note before ever having left the web site.',                      // Note to the merchant.  255 char max.
+            'allowedpaymentmethod' => '',           // The payment method type.  Specify the value InstantPaymentOnly.
+            'paymentaction' => 'Sale',                  // How you want to obtain the payment.  When implementing parallel payments, this field is required and must be set to Order.
+            'paymentrequestid' => '',               // A unique identifier of the specific payment request, which is required for parallel payments.
+            'sellerpaypalaccountid' => ''           // A unique identifier for the merchant.  For parallel payments, this field is required and must contain the Payer ID or the email address of the merchant.
+        );
+
+        $PaymentOrderItems = array();
+        foreach(Cart::content() as $row):
+            $Item = array(
+                'name' => $row->name,
+                'desc' => $row->name,
+                'amt' => $row->price,
+                'number' => $row->id,
+                'qty' => $row->qty
+            );
+            array_push($PaymentOrderItems, $Item);
+        endforeach;
+
+        $Payment['order_items'] = $PaymentOrderItems;
+        array_push($Payments, $Payment);
+
+        $PayPalRequest = array(
+           'DECPFields' => $DECPFields,
+           'Payments' => $Payments
+        );
+
+        $data = $this->PayPal->DoExpressCheckoutPayment($PayPalRequest);
+        Cart::destroy();
+        session()->forget('SetExpressCheckoutResult');
+        return view('front.ecommerce.checkout-thankyou', compact('data'));
+    }
+
+    /**
+     * Show checkout fail page
+     */
+    public function showCheckoutFail(){
+        return view('front.404');
+    }
+}
--- resources/views/front/ecommerce/checkout.blade.php
+++ resources/views/front/ecommerce/checkout.blade.php
@@ -26,121 +26,125 @@
         <div class="container">
             {!! Form::open(['route' => 'front.checkout.post', 'class' => 'form', 'method' => 'POST']) !!}
             <div class="row">
-                <h2 class="text-uppercase">Check out</h2>
-                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingOne">
-                            <h4 class="panel-title text-uppercase">
-                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    <span>+</span>Billing and Shipping Information
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                            <div class="panel-body">
-                                <div class="col-md-6">
-                                    <h2>Billing information</h2>
-                                    @include('partials.input-text', ['field' => 'billingFirstName', 'label' => 'First name'])
-                                    @include('partials.input-text', ['field' => 'billingLastName', 'label' => 'Last name'])
-                                    @include('partials.input-text', ['field' => 'billingCompany', 'label' => 'Company'])
-                                    @include('partials.input-text', ['field' => 'billingAddress1', 'label' => 'Address 1'])
-                                    @include('partials.input-text', ['field' => 'billingAddress2', 'label' => 'Address 2'])
-                                    @include('partials.input-text', ['field' => 'billingPostCode', 'label' => 'Postcode / ZIP'])
-                                    @include('partials.input-text', ['field' => 'billingCity', 'label' => 'Town / City'])
-                                    @include('partials.input-text', ['field' => 'billingPhone', 'label' => 'Phone'])
-                                    @include('partials.input-text', ['field' => 'billingEmail', 'label' => 'Email'])
+                <h1 class="text-uppercase">Shopping cart</h1>
+                    @if( Cart::count() )
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title text-uppercase">
+                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            <span>+</span>Billing and Shipping Information
+                                        </a>
+                                    </h4>
                                 </div>
-                                <div class="col-md-6">
-                                    <h2>Shipping information</h2>
-                                    @include('partials.input-text', ['field' => 'shippingFirstName', 'label' => 'First name'])
-                                    @include('partials.input-text', ['field' => 'shippingLastName', 'label' => 'Last name'])
-                                    @include('partials.input-text', ['field' => 'shippingCompany', 'label' => 'Company'])
-                                    @include('partials.input-text', ['field' => 'shippingAddress1', 'label' => 'Address 1'])
-                                    @include('partials.input-text', ['field' => 'shippingAddress2', 'label' => 'Address 2'])
-                                    @include('partials.input-text', ['field' => 'shippingPostCode', 'label' => 'Postcode / ZIP'])
-                                    @include('partials.input-text', ['field' => 'shippingCity', 'label' => 'Town / City'])
-                                    @include('partials.input-text', ['field' => 'shippingPhone', 'label' => 'Phone'])
-                                    @include('partials.input-text', ['field' => 'shippingEmail', 'label' => 'Email'])
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingFour">
-                            <h4 class="panel-title text-uppercase">
-                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour" class="text-uppercase">
-                                    <span>+</span>Payment Method
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
-                            <div class="panel-body">
-                                <div class="form-group">
-                                    <div class="radio">
-                                        <label for="payment-paypal"><input type="radio" checked value="paypal" name="paymentMethod">PayPal</label>
+                                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                    <div class="panel-body">
+                                        <div class="col-md-6">
+                                            <h2>Billing information</h2>
+                                            @include('partials.input-text', ['field' => 'billingFirstName', 'label' => 'First name'])
+                                            @include('partials.input-text', ['field' => 'billingLastName', 'label' => 'Last name'])
+                                            @include('partials.input-text', ['field' => 'billingCompany', 'label' => 'Company'])
+                                            @include('partials.input-text', ['field' => 'billingAddress1', 'label' => 'Address 1'])
+                                            @include('partials.input-text', ['field' => 'billingAddress2', 'label' => 'Address 2'])
+                                            @include('partials.input-text', ['field' => 'billingPostCode', 'label' => 'Postcode / ZIP'])
+                                            @include('partials.input-text', ['field' => 'billingCity', 'label' => 'Town / City'])
+                                            @include('partials.input-text', ['field' => 'billingPhone', 'label' => 'Phone'])
+                                            @include('partials.input-text', ['field' => 'billingEmail', 'label' => 'Email'])
+                                        </div>
+                                        <div class="col-md-6">
+                                            <h2>Shipping information</h2>
+                                            @include('partials.input-text', ['field' => 'shippingFirstName', 'label' => 'First name'])
+                                            @include('partials.input-text', ['field' => 'shippingLastName', 'label' => 'Last name'])
+                                            @include('partials.input-text', ['field' => 'shippingCompany', 'label' => 'Company'])
+                                            @include('partials.input-text', ['field' => 'shippingAddress1', 'label' => 'Address 1'])
+                                            @include('partials.input-text', ['field' => 'shippingAddress2', 'label' => 'Address 2'])
+                                            @include('partials.input-text', ['field' => 'shippingPostCode', 'label' => 'Postcode / ZIP'])
+                                            @include('partials.input-text', ['field' => 'shippingCity', 'label' => 'Town / City'])
+                                            @include('partials.input-text', ['field' => 'shippingPhone', 'label' => 'Phone'])
+                                            @include('partials.input-text', ['field' => 'shippingEmail', 'label' => 'Email'])
+                                        </div>
                                     </div>
-                                    <p class="description"><i>We only allow PayPal method right now!</i></p>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingFour">
+                                    <h4 class="panel-title text-uppercase">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour" class="text-uppercase">
+                                            <span>+</span>Payment Method
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+                                    <div class="panel-body">
+                                        <div class="form-group">
+                                            <div class="radio">
+                                                <label for="payment-paypal"><input type="radio" checked value="paypal" name="paymentMethod">PayPal</label>
+                                            </div>
+                                            <p class="description"><i>We only allow PayPal method right now!</i></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingFive">
+                                    <h4 class="panel-title text-uppercase">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive" class="text-uppercase">
+                                            <span>+</span>Order Review
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                                    <div class="panel-body">
+                                        <h3>Your order detail</h3>
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="product-name">Product</th>
+                                                    <th class="product-total">Total</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach(Cart::content() as $row)
+                                                <tr class="cart_item">
+                                                    <td class="product-name">
+                                                        {{ $row->name }}&nbsp;
+                                                        <strong class="product-quantity">× {{ $row->qty }}</strong>
+                                                    </td>
+                                                    <td class="product-total">
+                                                        ${{ number_format( $row->qty * $row->price, 2) }}
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+
+                                            </tbody>
+                                            <tfoot>
+
+                                            <tr class="cart-subtotal">
+                                                <th>Subtotal</th>
+                                                <td>${{ Cart::subtotal() }}</td>
+                                            </tr>
+                                            <tr class="shipping">
+                                                <th>Tax</th>
+                                                <td>
+                                                    ${{ Cart::tax() }}
+                                                </td>
+                                            </tr>
+                                            <tr class="order-total">
+                                                <th>Total</th>
+                                                <td><span>${{ Cart::total() }}</span></td>
+                                            </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingFive">
-                            <h4 class="panel-title text-uppercase">
-                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive" class="text-uppercase">
-                                    <span>+</span>Order Review
-                                </a>
-                            </h4>
+                        <div class="button">
+                            <input class="btn btn-default" type="submit" value="Place order" />
                         </div>
-                        <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
-                            <div class="panel-body">
-                                <h3>Your order detail</h3>
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th class="product-name">Product</th>
-                                            <th class="product-total">Total</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach(Cart::content() as $row)
-                                        <tr class="cart_item">
-                                            <td class="product-name">
-                                                {{ $row->name }}&nbsp;
-                                                <strong class="product-quantity">× {{ $row->qty }}</strong>
-                                            </td>
-                                            <td class="product-total">
-                                                ${{ number_format( $row->qty * $row->price, 2) }}
-                                            </td>
-                                        </tr>
-                                    @endforeach
-
-                                    </tbody>
-                                    <tfoot>
-
-                                    <tr class="cart-subtotal">
-                                        <th>Subtotal</th>
-                                        <td>${{ Cart::subtotal() }}</td>
-                                    </tr>
-                                    <tr class="shipping">
-                                        <th>Tax</th>
-                                        <td>
-                                            ${{ Cart::tax() }}
-                                        </td>
-                                    </tr>
-                                    <tr class="order-total">
-                                        <th>Total</th>
-                                        <td><span>${{ Cart::total() }}</span></td>
-                                    </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="button">
-                    <input class="btn btn-default" type="submit" value="Place order" />
-                </div>
+                    @else
+                        <h3>Your cart is currently empty.</h3>
+                    @endif
             </div> <!-- .row -->
             {!! Form::close() !!}
         </div> <!-- .container -->
