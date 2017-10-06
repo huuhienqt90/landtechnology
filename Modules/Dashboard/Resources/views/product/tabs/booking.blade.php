4280667e6324a83ebb73da90c1e07fb674d9503f
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2017-10-06 09:47:40 +0700

    Update

---
+++ Modules/Dashboard/Resources/views/product/tabs/booking.blade.php
@@ -0,0 +1,20 @@
+<div class="booking-item form-horizontal" style="margin: 40px 0;">
+    <div class="form-group">
+        <label class="control-label col-md-2">Original Price</label>
+        <div class="col-md-4">
+            <input type="text" name="price" value="0" class="form-control" />
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-md-2">Sale Price</label>
+        <div class="col-md-4">
+            <input type="text" name="sale_price" value="0" class="form-control" />
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-md-2">Sale Price</label>
+        <div class="col-md-4">
+            <input type="text" name="sale_price" value="0" class="form-control" />
+        </div>
+    </div>
+</div>
